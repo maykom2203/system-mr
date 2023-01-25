14fd8c2e1b13d3e2b25028ec1e021aa046889fe4
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2023-01-25 00:03:31 -0300

    requisitos do 1 ao 8 passando sem erros de lint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,14 +3,15 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Customer from './pages/Customer';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
-      {/* <Route path="/customer" element={ <Customer /> } /> */}
       <Route path="/customer/products" element={ <Customer /> } />
+      <Route path="/register" element={ <Register /> } />
     </Routes>
   );
 }
