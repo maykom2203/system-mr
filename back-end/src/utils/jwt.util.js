39b19101af0aa969cfc1944bf2f28c636cf4b214
Luciana Alves <dra.luciana.alves@icloud.com>
2023-01-26 13:00:48 -0300

    Requisitos customer_products.

--- back-end/src/utils/jwt.util.js
+++ back-end/src/utils/jwt.util.js
@@ -1,25 +1,27 @@
+const { sign, verify } = require('jsonwebtoken');
+const jwtKey = require('fs')
+  .readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
 require('dotenv/config');
-const jwt = require('jsonwebtoken');
-
 const createToken = (data) => {
-    const token = jwt.sign({ data }, process.env.JWT_SECRET, {
+    const token = sign(
+        { data }, jwtKey,
+        {
         expiresIn: '1d',
-        algorithm: 'HS256', 
-    });
+        algorithm: 'HS256',
+        },
+    );
     return token;
 };
-
-const validateToken = (token) => {
+const validateToken = (token = null) => {
+    if (!token) throw new Error('Token not found');
     try {
-        const { data } = jwt.verify(token, process.env.JWT_SECRET);
-
+        const { data } = verify(token, process.env.JWT_SECRET || 'secret_key');
         return data;
     } catch (error) {
         const e = new Error('Expired or invalid token');
         return e;
     }
 };
-
 module.exports = { createToken,
-   validateToken,
-  };
+    validateToken,
+};
