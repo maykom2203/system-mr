09ec9d42b6f801fd8e591f012a12068239d991ab
Luiz Barbosa <luizbarbosa@MacBook-Air-de-Luiz.local>
2023-01-27 11:26:41 -0300

    requisito 13

--- back-end/src/controllers/auth.controller.js
+++ back-end/src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ const login = async (req, res) => {
     return res.status(404).json(validate);
   } 
   
-    return res.status(200).json({ token: validate.token, name: validate.name });
+    return res.status(200).json({ token: validate.token, name: validate.name, email: validate.email, role: validate.role });
 };
 
 module.exports = { login };
